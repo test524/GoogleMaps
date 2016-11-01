305e63ff743d89cc62f585414da6c8bf75bbecb6
Pavan Kumar Reddy <pavankumarreddy@Pavans-MacBook-Pro.local>
2016-11-01 14:16:14 +0530

    Initial Commit

---
+++ GoogleMapsDemo12/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleMapsDemo12
+//
+//  Created by Pavan Kumar Reddy on 01/11/16.
+//  Copyright © 2016 Softobiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
